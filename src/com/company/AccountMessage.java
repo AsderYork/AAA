4038eb0c0f6cb239add8caba3cc46afe24e5e126
AsderYork <fake14mail@gmail.com>
2016-09-16 18:18:18 +0700

    asd (#4)
    Beginning of work

---
+++ src/com/company/AccountMessage.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by York on 16.09.2016.
+ */
+public class AccountMessage {
+    int USERID;
+    String Date;
+    String Action; //Login/AccessGranted/AccessRejected
+    String Role;
+    String PATH;
+    int Value;
+    String DateStart;
+    String DateFinished;
+}
