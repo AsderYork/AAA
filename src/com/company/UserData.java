fed6acf9cb80642ef015bc8689704a4395f23e86
Asder York <fake14mail@gmail.com>
2016-09-16 18:13:09 +0700

    Auto stash for revert of "Revert "Beginning of work""

---
+++ src/com/company/UserData.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by York on 16.09.2016.
+ */
+public class UserData {
+    String Username;
+    String Name;
+    String HashedPassword;
+    String Salt;
+    int ID;
+}
