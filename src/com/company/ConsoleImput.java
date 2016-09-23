72460e071d5a83fba9e4aedde2306ac31953da7b
spacecrio <spacecrio@users.noreply.github.com>
2016-09-24 01:50:46 +0700

    actialy auto fmt

---
+++ src/com/company/ConsoleImput.java
@@ -0,0 +1,22 @@
+/*
+package com.company;
+
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+
+public class ConsoleImput {
+    private String[] cl = null;
+
+    Options options = new Options();
+
+    CommandLineParser parser = new DefaultParser();
+
+    public ConsoleImput(String[] args, UserInput ui) {
+
+        cl = args;
+    }
+
+    public int parse() {
+    }
+*/
