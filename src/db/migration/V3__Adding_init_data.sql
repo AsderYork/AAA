ac96f81f52faa942d4442e322d655c50aac31cd4
Asder York <fake14mail@gmail.com>
2016-12-06 21:02:42 +0700

    Now data is realy added

--- src/db/migration/V3__Adding_init_data.sql
+++ src/db/migration/V3__Adding_init_data.sql
@@ -1,1 +1,14 @@
+INSERT INTO USERS_DATA(Login, Username, HashedPassword, Salt)
+VALUES('jdoe','John Doe','67afaf691096743173e2bc3a5b8266c9', 'Salt' );
+INSERT INTO USERS_DATA(Login, Username, HashedPassword, Salt)
+VALUES('jrow','Jane Row','28f5f0da487d14868db9fe2b00042739', 'AnotherSalt');
 
+
+INSERT INTO PERMISSION_SDATA(ID, Subresource, Permission)
+VALUES(1,'a',1);
+INSERT INTO PERMISSION_SDATA(ID, Subresource, Permission)
+VALUES(1,'a.b',2);
+INSERT INTO PERMISSION_SDATA(ID, Subresource, Permission)
+VALUES(2,'a.b.c',4);
+INSERT INTO PERMISSION_SDATA(ID, Subresource, Permission)
+VALUES(1,'a.bc',4);
