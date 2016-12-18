62886dc0d1919e76530e8ea5d6b3becb65812de8
Asder York <fake14mail@gmail.com>
2016-12-18 19:22:09 +0700

    Release 6 is ready

--- AAAProjects.web/src/main/java/com/triplea/Servlets/ActivityServlet.java
+++ AAAProjects.web/src/main/java/com/triplea/Servlets/ActivityServlet.java
@@ -1,5 +1,6 @@
 package com.triplea.Servlets;
 
+import javax.inject.Singleton;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Created by Asder on 12.12.2016.
  */
+@Singleton
 public class ActivityServlet  extends HttpServlet {
 
     protected void service(HttpServletRequest req, HttpServletResponse resp) {
